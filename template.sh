4f3183692e2de172c40c08cbc1afc6c5960f3929
SabyaSachee-AI <sabyasachee.ds@gmail.com>
2026-04-17 13:29:39 +0600

    update

---
+++ template.sh
@@ -0,0 +1,12 @@
+#creating directory for the project
+mkdir -p src
+
+#creating files for the project 
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch store_index.py
+echo "directory and files created successfully!."
