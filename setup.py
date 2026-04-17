ece593062c60b4ea7cc9532e4d027c09ba2744d8
SabyaSachee-AI <sabyasachee.ds@gmail.com>
2026-04-17 13:59:56 +0600

    update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Medical_chatbot',
+    version='0.1',
+    author='SabyaSacheeDas',
+    authoremail='sabyasachee.ds@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+
+
+
+)
+
